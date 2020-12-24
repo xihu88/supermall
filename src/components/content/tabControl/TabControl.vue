c0af9d744e2da337a19a31a1547404d7404433a1
xihu88 <shan888fly@163.com>
2020-12-24 18:42:56 +0800

    tabcontrul

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="tab-control">
+    <div class="tab-control-item" :class="{active:index==currentIndex}" @click="itemClick(index)" v-for="(item,index) in title" :key="item"> {{item}} </div>
+  </div>
+</template>
+<script>
+  export default {
+    name:'TabControl',
+    props: {
+      title:Array
+    },
+    data () {
+      return {
+        currentIndex: 0
+      }
+    },
+    methods: {
+      itemClick(index){
+        this.currentIndex = index
+      }
+    }
+  }
+</script>
+<style scoped>
+  #tab-control {
+    display: flex;
+    justify-content: space-around;
+  }
+  .tab-control-item {
+    /* flex: 1; */
+    padding: 0 10px;
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+  }
+  .active {
+    color: var(--color-high-text);
+    border-bottom: 3px solid var(--color-high-text);
+  }
+</style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="RecommendView">
+    <div class="recommenditem" v-for="item in recommends" :key="item.title" >
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <span> {{item.title}} </span>
+      </a> 
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name:'RecommendView',
+    props: {
+      recommends: Array
+    }
+  }
+</script>
+<style scoped>
+  #RecommendView {
+    display: flex;
+    justify-content: space-around;
+    text-align: center;
+    padding: 10px 0;
+    box-shadow: 0 0px 5px rgba(0, 0, 0, 0.2);
+  }
+  .recommenditem {
+    flex: 1;
+    font-size: 14px;
+    line-height: 20px;
+  }
+  .recommenditem img {
+    height: 65px;
+    width: 65px;
+  }
+
+</style>
